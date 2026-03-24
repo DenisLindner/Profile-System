145f076b88248a0ba8834951a061c4aaef04a586
Denis Lindner <lindnerdenis19@gmail.com>
2026-03-24 12:23:56 -0300

    feat: CurrentUser Decorator configured

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,22 +1,25 @@
-import { Controller, Delete, Get, Patch } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Patch } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CurrentUserDecorator } from 'src/auth/decorators/current-user.decorator';
+import type { UserPayload } from './dto/user-payload.dto';
+import { UpdateUserDTO } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly service: UsersService) {}
 
     @Get('me')
-    async getProfile() {
-        
+    async getProfile(@CurrentUserDecorator() user: UserPayload) {
+        return this.service.findById(user.sub);
     }
 
     @Patch('me')
-    async updateProfile() {
-        
+    async updateProfile(@CurrentUserDecorator() user: UserPayload, @Body() dto: UpdateUserDTO) {
+        return this.service.update(user.sub, dto)
     }
 
     @Delete('me')
-    async deleteProfile() {
-        
+    async deleteProfile(@CurrentUserDecorator() user: UserPayload) {
+        return this.service.deleteById(user.sub)
     }
 }
