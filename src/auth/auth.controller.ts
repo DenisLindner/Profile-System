72cc8d57390ba04aa68ebf4db2db068381a2c1ac
Denis Lindner <lindnerdenis19@gmail.com>
2026-03-24 12:26:14 -0300

    feat: AuthController configured

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { IsPublic } from './decorators/is-public.decorator';
+import { AuthService } from './auth.service';
+import { CreateUserDTO } from 'src/users/dto/create-user.dto';
+import { LoginDTO } from './dto/login.dto';
 
 @Controller('auth')
 @IsPublic()
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly service: AuthService) {}
+
+    @Post('register')
+    async register(@Body() dto: CreateUserDTO) {
+        return this.service.register(dto)
+    }
+
+    @Post('login')
+    @HttpCode(200)
+    async login(@Body() dto: LoginDTO) {
+        return this.service.login(dto)
+    }
+}
